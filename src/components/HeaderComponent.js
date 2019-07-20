1f2cd7d89fbd9955e70c92b6d7577f8f801c773e
cesar18pena <cesar18pena@gmail.com>
2019-07-20 14:46:54 -0400

    Fix: solve problems with styles in header component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
-import '../App.css'
 
 class Header extends Component {
-
   constructor(props) {
     super(props);
 
@@ -24,42 +22,40 @@ class Header extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar dark >
+        <Navbar dark expand="md">
           <div className="container">
             <NavbarToggler onClick={this.toggleNav} />
-            <NavbarBrand className="mr-auto" href="/">
-              <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
-            </NavbarBrand>
-            <Collapse isOpen={this.state.isNavOpen} navbar />
-            <Nav navbar>
-              <NavItem>
-                <NavLink className="nav-link" to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className="nav-link" to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
-              </NavItem>
-            </Nav>
+            <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
           </div>
         </Navbar>
         <Jumbotron>
           <div className="container">
             <div className="row row-header">
               <div className="col-12 col-sm-6">
-                <h1>Ristorante Con Fusion</h1>
-                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience.
-                  Our lipsmacking creations will tickle your culinary senses!</p>
+                <h1>Ristorante con Fusion</h1>
+                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
               </div>
             </div>
           </div>
         </Jumbotron>
       </React.Fragment>
-    )
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './index.css';
+
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-social/bootstrap-social.css';
-import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
