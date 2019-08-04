a1d79b8103a5bdc1892b940d3ede795a4514beef
cesar18pena <cesar18pena@gmail.com>
2019-08-03 21:49:37 -0400

    Feat: use react router dom to pass parameters and add breadcumb to some components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,61 +1,79 @@
-import React from 'react'
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({ dish }) {
-  if (dish != null)
+  if (dish != null) {
     return (
       <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
         <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>
         </CardBody>
       </Card>
-    );
-  else
+    )
+  } else {
     return (
       <div></div>
     );
+  }
 }
 
 function RenderComments({ comments }) {
-  if (comments != null)
+  const options = { year: 'numeric', month: 'short', day: 'numeric' };
+  if (comments != null) {
+    const commentaries = comments.map((comment) => {
+      return (
+        <div key={comment.id}>
+          <ul className="list-unstyled">
+            <li className="media">
+              <div className="media-body">
+                <p>{comment.comment}</p>
+                <p>-- {comment.author}, {new Date(comment.date).toLocaleDateString('en-US', options)}</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      );
+    });
+
     return (
-      comments.map((comment) => {
-        return (
-          <div key={comment.id}>
-            <h3>Comments</h3>
-            <ul className="list-unstyled">
-              <li>{comment.comment}</li>
-              <li>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</li>
-            </ul>
-          </div>
-        )
-      })
+      <div>
+        <h4>Comments</h4>
+        {commentaries}
+      </div>
     );
-  else
+  } else {
     return (
       <div></div>
     );
+  }
 }
 
 const DishDetail = (props) => {
-  if (props.dish != null)
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            <RenderDish dish={props.dish} />
-          </div>
-          <div className="col-12 col-md-5 m-1">
-            <RenderComments comments={props.dish.comments} />
-          </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem><Link to="/menu">menu</Link></BreadcrumbItem>
+          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>{props.dish.name}</h3>
+          <hr />
         </div>
-      </div>)
-  else
-    return (
-      <div></div>
-    );
+      </div>
+      <div className="row">
+        <div className="col-sm-12 col-md-5 m-1">
+          <RenderDish dish={props.dish} />
+        </div>
+        <div className="col-sm-12 col-md-5 m-1">
+          <RenderComments comments={props.comments} />                    
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DishDetail;
